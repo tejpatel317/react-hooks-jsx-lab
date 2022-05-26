29bf639b391cb1b0336ca2e05b425b81631991cf
Tej Patel <tejpatel317@gmail.com>
2022-05-26 18:51:20 -0400

    commit tree 83d00646c45c1b980f51f6fca7533b739cebe190

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const divStyle = {
+    color: "firebrick"
+  }
+  return (
+    <div id="home">
+      <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+    </div>
+    )
 }
 
 export default Home;
